648988caba07920240fcad65bdd035094bb83871
chrizzbln89 <christoph.bieritz@hotmail.de>
2024-05-21 03:33:03 +0000

    Tue May 21 03:33:03 UTC 2024

---
+++ locust.py
@@ -0,0 +1,16 @@
+from locust import HttpUser, TaskSet, task, between
+
+
+class UserBehavior(TaskSet):
+    @task
+    def get_data(self):
+        self.client.get("/data")
+
+    @task
+    def post_data(self):
+        self.client.post("/data", json={"key": "value"})
+
+
+class WebsiteUser(HttpUser):
+    tasks = [UserBehavior]
+    wait_time = between(1, 5)
